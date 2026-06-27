ae7cf9b30330c728a9ac29a8a934fc75f1832e92
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-06-27 14:21:53 +0500

    fix: swagger

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceHttps();
         }
+
+        Gate::define('viewSwaggerDocs', function ($user) {
+            return in_array($user->email, ['uniqdeveloper@yandex.ru']);
+        });
 
         Vite::prefetch(concurrency: 3);
     }
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,11 +10,11 @@ php artisan route:cache
 php artisan view:cache
 php artisan event:cache
 
+# Docs
+php artisan l5-swagger:generate --ansi --no-interaction || true
+
 # Create storage link if not exists
 php artisan storage:link --ansi --no-interaction || true
-
-# Docs
-php artisan l5-swagger:generate
 
 # Execute the main command
 exec "$@"
