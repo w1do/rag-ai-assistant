19a27fd71472a82740c1f2758e880c873e3b71e4
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-06-29 01:50:31 +0500

    fix: nginx

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\Assistant\Api\V1\AssistantCallbackController;
 use App\Http\Controllers\Assistant\Api\V1\SearchAssistantController;
 use App\Http\Controllers\Chat\Api\ChatController;
+use App\Http\Controllers\Chat\PublicChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
+
     Route::get('/chats', [ChatController::class, 'index']);
 
     Route::prefix('v1')->group(function () {
