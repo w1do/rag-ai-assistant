a9d601f7fdeee9bc73b05f00c03da92114230842
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-06-29 01:06:14 +0500

    fix: dashboard route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::prefix('cabinet')->group(function () {
+Route::prefix('/')->group(function () {
     Route::get('/chats', [MarketplaceController::class, 'index'])->name('chats.index');
 
     Route::middleware(['auth', 'verified'])->group(function () {
