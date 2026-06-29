5fa9bbacce009ad4d0eae03f03bc54a98ed1e5da
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-06-29 14:19:30 +0500

    fix: web

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::prefix('/dashboard')->group(function () {
+Route::prefix('/')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
    // Route::get('/chats', [MarketplaceController::class, 'index'])->name('chats.index');
 
     Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
         Route::resource('assistants', AssistantController::class);
         Route::post('assistants/{assistant}/upload-document', [AssistantController::class, 'uploadDocument'])->name('assistants.upload-document');
